f874c16e41168abbf68b7d58864a78d07a39c6f7
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-02-13 16:17:26 +0900

    Hello World

---
+++ 입출력과 사칙연산/P_2557.java
@@ -0,0 +1,19 @@
+package 입출력과사칙연산;
+
+/*[2557] Hello World
+1. 문제 및 제약조건
+:	Hello World!를 출력하시오.
+2. 입력
+:	없음
+3. 출력
+:	Hello World!를 출력하시오.
+*/
+
+public class P_2557 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
