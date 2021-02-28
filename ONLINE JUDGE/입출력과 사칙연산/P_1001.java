faafe293fe5a36603ec7b2d3b235269d67c77b9a
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-02-28 22:48:41 +0900

    A-B

---
+++ ONLINE JUDGE/입출력과 사칙연산/P_1001.java
@@ -0,0 +1,24 @@
+package 입출력과사칙연산;
+import java.util.Scanner;
+
+/*[1001] A-B
+1. 문제 및 제약조건
+:	두 정수 A와 B를 입력받은 다음, A-B를 출력하는 프로그램을 작성하시오.
+2. 입력
+:	첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+3. 출력
+:	첫째 줄에 A-B를 출력한다.
+*/
+
+public class P_1001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a-b);
+		
+	}
+
+}
